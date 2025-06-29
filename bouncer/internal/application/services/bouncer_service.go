d97a808144b9b3c95a44b4e3e36e59c9dc84fb9f
Clément TARIEL <clement.tariel@gmail.com>
2025-06-29 19:47:51 +0200

    feat: WIP add frontend + add highlighted match endpoint

--- bouncer/internal/application/services/bouncer_service.go
+++ bouncer/internal/application/services/bouncer_service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"time"
+
 	"github.com/ClementTariel/rg-lua/bouncer/internal/domain/entities"
 	"github.com/ClementTariel/rg-lua/bouncer/internal/domain/external"
 	"github.com/ClementTariel/rg-lua/bouncer/internal/domain/repositories"
@@ -9,9 +11,10 @@ import (
 )
 
 type BouncerService struct {
-	botRepo      repositories.BotRepository
-	matchRepo    repositories.MatchRepository
-	matchmakerMS external.MatchmakerMS
+	botRepo          repositories.BotRepository
+	matchRepo        repositories.MatchRepository
+	matchmakerMS     external.MatchmakerMS
+	highlightedMatch *entities.Match
 }
 
 func NewBouncerService(botRepo repositories.BotRepository, matchRepo repositories.MatchRepository) BouncerService {
@@ -32,4 +35,32 @@ func (s *BouncerService) GetMatch(matchId uuid.UUID) (entities.Match, error) {
 
 func (s *BouncerService) GetSummaries(start int, size int) ([]entities.MatchSummary, error) {
 	return s.matchRepo.GetSummaries(start, size)
+}
+
+func (s *BouncerService) GetHighlightedMatch() (*entities.Match, error) {
+	defer s.GetHighlightedMatchDebounced()
+	if s.highlightedMatch != nil {
+		return s.highlightedMatch, nil
+	}
+	return s.GetHighlightedMatchDebounced()
+}
+
+func (s *BouncerService) GetHighlightedMatchDebounced() (*entities.Match, error) {
+	if s.highlightedMatch == nil || s.highlightedMatch.Date.Add(time.Hour*12).Before(time.Now()) {
+		// TODO: find some metrics to discover interesting matchs
+		summaries, err := s.GetSummaries(1, 1)
+		if err != nil {
+			return nil, err
+		}
+		if len(summaries) == 0 {
+			return nil, nil
+		}
+		matchId := summaries[0].Id
+		match, err := s.GetMatch(matchId)
+		if err != nil {
+			return nil, err
+		}
+		return &match, nil
+	}
+	return s.highlightedMatch, nil
 }
